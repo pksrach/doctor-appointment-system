02f9468933b1717c1275e307a4edcd73c6df5d8f
ReachbundithOum <benjason8888@gmail.com>
2024-01-10 22:19:54 +0700

    modify on about us page

--- resources/views/frontend/about-us.blade.php
+++ resources/views/frontend/about-us.blade.php
@@ -18,6 +18,24 @@
     </div>
     <!-- /Breadcrumb -->
     {{-- Contain --}}
-    <h1 style="padding: 50px 20px">Contain here</h1>
+    <div class="top-content container">
+        <h4 class="mt-5 text-info text-bold">Our Vision & Mission</h4> <hr />
+            <div class="custom-card row pt-4 mb-5" style="height: 270px;">
+                <div class="col-12 d-flex justify-content-center flex-wrap-reverse">
+                    <div class="col-lg-5 col-md-6 col-12 shadow p-5" style="z-index: 200;border-radius: 0px 0px 5px 5px;">
+                        <div class="row">
+                            <div class="col-10">
+                                <p style="font-size:10.7px;color:gray;">
+                                    We aim to offer the same quality of service and medical excellence just like Japan Hospitals.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-6 col-12 p-0 d-inline">
+                        <img src="assets\img\hospital-01.jpg" alt="" style="width:100%" />
+                    </div>
+                </div>
+            </div>
+    </div>
     {{-- End of Contain --}}
 @endsection
